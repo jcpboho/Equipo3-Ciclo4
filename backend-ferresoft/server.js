687c15ee7f088350bed58934461916ffdfeb7fa4
J K <camilopa1998@hotmail.com>
2021-11-03 22:10:20 -0500

    Vista about y contact

--- backend-ferresoft/server.js
+++ backend-ferresoft/server.js
@@ -43,6 +43,6 @@ const PORT = process.env.PORT || 3000;
 
 //Iniciar el servidor
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
 
--- frontend-ferresoft/src/views/Public/Contact.js
+++ frontend-ferresoft/src/views/Public/Contact.js
@@ -1,11 +1,27 @@
 
 //Importamos el componente navbar para mostrar los enlaces de navegación
 import NavBar from "../../components/NavBar";
+
+import "../../assets/css/contact.css"
 const Contact = () => (
     <>
         {/* Utilizamos el componente navbar el cual tiene los enlaces de navegación */}
         <NavBar />
-        <h1>Contact</h1>
+        <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+            <header className="mb-auto"></header>
+
+            <main className="px-3">
+                <h1>Contactenos</h1>
+                <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the
+                    text, and add your own fullscreen background photo to make it your own.</p>
+                <p className="lead">
+                    <a href="#s" className="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+                </p>
+            </main>
+
+            <footer className="mt-auto text-white-50">
+            </footer>
+        </div>
     </>
 );
 
